ae0f6516a6f684c12952d6af8d142dae7ae950ec
macanudo527 <macanudo@yahoo.com>
2016-06-07 10:01:35 +0900

    Add question update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,6 +13,24 @@ class QuestionsController < ApplicationController
   def edit
     @question = Question.find(params[:id])
   end
+  
+  def update
+    @question = Question.find(params[:id])
+    if @question.update_attributes(question_params)
+      flash[:success] = "Question successfully updated."
+      redirect_to @question.grouping
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def question_params
+      params.require(:question).permit(:question, :correct_answer, :a1,
+                                   :a2, :a3)
+    end
+
 
   
 end

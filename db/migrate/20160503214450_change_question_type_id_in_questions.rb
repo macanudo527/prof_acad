e512db5ddb789db10f5f674ecc6681e154fe9e23
macanudo527 <macanudo@yahoo.com>
2016-06-08 10:06:08 +0900

    optimize db migrations

--- db/migrate/20160503214450_change_question_type_id_in_questions.rb
+++
@@ -1,8 +0,0 @@
-class ChangeQuestionTypeIdInQuestions < ActiveRecord::Migration
-  def change
-        change_table :questions do |t|
-          t.rename :question_type_id, :question_type
-        end
-        add_reference :questions, :question_type, index:true, foreign_key: true
-  end
-end
--- db/migrate/20160608004407_remove_question_type_from_questions.rb
+++
@@ -1,5 +0,0 @@
-class RemoveQuestionTypeFromQuestions < ActiveRecord::Migration
-  def change
-    remove_column :questions, :question_type, :integer
-  end
-end

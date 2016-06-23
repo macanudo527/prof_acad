eff6a35dd0f02022cbadb2b53bdee8cf260f5726
macanudo527 <macanudo@yahoo.com>
2016-06-23 09:49:47 +0900

    Learn Rspec

--- spec/controllers/grouping_spec.rb
+++ spec/controllers/grouping_spec.rb
@@ -9,13 +9,28 @@ RSpec.describe GroupingsController do
     get :new
     expect {
       @grouping = Grouping.new(name: "")
-      @grouping.questions = Question.new(question: "", correct_answer: "", a2: "", a3: "", a4: "")
-      @grouping.create!
+      @grouping.questions.build(question: "", correct_answer: "", a2: "", a3: "", a4: "")
+      @grouping.save
 #      post groupings_path, { "grouping" => { name: "", "question_attributes" => "" }}
     }.to_not change{Question.count} 
+
+    @one_question =  [{question: "What is 1+1?", correct_answer: "0", a2: "1", a3: "2", a4: "3"},
+                      {question: "", correct_answer: "", a2: "", a3: "", a4: ""}]
+    
+     expect {
+      @grouping = subject.current_user.groupings.build(@one_question)
+      @grouping.questions.each do |question|
+        question.user = subject.current_user      
+      end
+      @grouping.save
+#      post groupings_path, { "grouping" => { name: "", "question_attributes" => "" }}
+    }.to change{Question.count}.by(1)    
+    
   end
     
-    
+  def grouping_params
+ #   params.require(:grouping).permit(:name, questions_attributes: [:id, :grouping_id, :user_id, :question, :correct_answer, :a2, :a3, :a4])
+  end
     
     
     

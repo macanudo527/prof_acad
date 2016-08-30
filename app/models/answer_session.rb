8e0c1e7b4a9c743d2feb7c326d9f7fbbd75ff860
macanudo527 <macanudo@yahoo.com>
2016-08-31 08:33:56 +0900

    Reform bubbling error messages

--- app/models/answer_session.rb
+++ app/models/answer_session.rb
@@ -9,13 +9,29 @@ class AnswerSession < ActiveRecord::Base
     :reject_if     => :all_blank
   
    validate do |answer_session|
-    answer_session.user_answers.each do |user_answer|
+    error_msg = "Missing Answer for Questions "
+    questions_with_errors = Array.new()
+    count = 0
+    answer_session.user_answers.each_with_index do |user_answer, index|
       next if user_answer.valid?
-      user_answer.errors.full_messages.each do |msg|
+ #     user_answer.errors.full_messages.each do |msg|
         # you can customize the error message here:
-        errors[:base] << "Missing Answer for Question '#{user_answer.question.question}'"
+        questions_with_errors.push(index+1)
+        count++
+        errors[:base] << "Missing Answer for Questions #{user_answer.question.question}"
+ #     end
+    end
+    questions_with_errors.each_with_index do |questionno, index|
+      if index < count - 2
+        error_msg += questionno + ", "
+      elsif index == count - 2
+        error_msg += questionno
+      else
+        error_msg += " and " + questionno
       end
     end
+    
   end
   
 end
+

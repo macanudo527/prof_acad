90ebfa391f4da5e60155a56913a63cd25856ec45
macanudo527 <macanudo@yahoo.com>
2016-08-30 14:47:36 +0900

    Fix bubbling validation

--- app/models/answer_session.rb
+++ app/models/answer_session.rb
@@ -7,7 +7,15 @@ class AnswerSession < ActiveRecord::Base
   accepts_nested_attributes_for :user_answers,
     :allow_destroy => true,
     :reject_if     => :all_blank
-    
-  validates_associated_bubbling :user_answers
+  
+   validate do |answer_session|
+    answer_session.user_answers.each do |user_answer|
+      next if user_answer.valid?
+      user_answer.errors.full_messages.each do |msg|
+        # you can customize the error message here:
+        errors[:base] << "Missing Answer for Question '#{user_answer.question.question}'"
+      end
+    end
+  end
   
 end

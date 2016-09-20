ca0809d4863b3833a03db153f29d8aa8771f5898
macanudo527 <macanudo@yahoo.com>
2016-09-20 22:00:22 +0900

    Add question from pool

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -3,7 +3,7 @@ class SharesController < ApplicationController
   def create
     grouping = Grouping.find(params[:grouping_id])
     grouping.addQuestionId(params[:question_id])
-    render grouping(params[:grouping_id])
+    redirect_to Grouping.find(params[:grouping_id])
   end
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
     resources :questions
     resources :answer_sessions
   end
+  
+  resources :shares
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
